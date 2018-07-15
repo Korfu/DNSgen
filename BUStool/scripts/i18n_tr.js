f4cee32f1ba9a0adabeb71f06a10ed22702ef6d8
Korfu <36454389+Korfu@users.noreply.github.com>
2018-07-15 14:07:05 +0200

    updating BUStool with portugese languange

---
+++ BUStool/scripts/i18n_tr.js
@@ -0,0 +1,311 @@
+var translate = {
+    en: {
+    m1: "BUStool",
+    m2: "DNS instructions",
+    m3: "Transfer message",
+    m4: "DNS verification",
+    m5: "Warm-up",
+    l1: "First name",
+    l2: "Domain name:",
+    l3: "Masking domain:",
+    l4: "First IP:",
+    l5: "Second IP:",
+    l6: "Third IP:",
+    l7: "Fourth IP:",
+    l8: "Spf tag:",
+    l9: "Service:",
+    l10: "Country:",
+    l11: "US",
+    l12: "China",
+    l13: "Brasil",
+    l14: "Russia",
+    l15: "Other",
+    l16: "B2B",
+    l17: "google-site-verification:",
+    l18: "DKIM:",
+    l19: "Mailbox type:",
+    l20: "IQ setup",
+    l21: "Own",
+    dns_english_btn: "[EN] dedicated IPs",
+    dns_dedicated_btn: "[PL] dedicated IPs",
+    dns_shared_btn: "[PL] shared IPs",
+    dns_verification: "DNS verification",
+    temp_pass_label: "ExS password:",
+    iq_pass_label: "IQ pass:",
+    pl: "Poland",
+    us: "United States",
+    uk: "United Kingdom",
+    fr: "France",
+    ru: "Russia",
+    it: "Italy",
+    ch: "China",
+    de: "Germany",
+    cz: "Czech Republic",
+    in: "INTERNATIONAL",
+    bustype: "Business type",
+    tr_en_transfer: "[EN] Transfer message",
+    tr_pl_transfer: "[PL] transfer message",
+    faq_q1: "What browser to use?",
+    faq_q2: "How to use the tool?",
+    faq_q3: "Something is wrong!",
+    faq_h1: "Google Chrome",
+    faq_h2: "Wisely",
+    faq_h3: "Report it!",
+    faq_a1: "Outlook for some reason interacts better with Chrome than Firefox. Therefore copying a table with styles will be only available when using Chrome.",
+    faq_a2: "The tool was made to help you with Business Unit Setup, however it doesn't have data validation fields. Make sure you take following into consideration:",
+    faq_a2_1:"1. Make sure to check previous SPF records for E-commerce customers and adjust records to needs of a client,",
+    faq_a2_2:"2. Always check if all the data is correct with DNS verification tool",
+    faq_a2_3:"3.  In DNS verifcation tool you will need to manually delete '/' sign from the DKIM key to make it work in 'verify DNS' panel in ExpertSender",
+    faq_a2_4: "4.  There is only version for four IPs in DNS instructions, make sure to check how many IPs are being assigned and adjust accordingly",
+    faq_a3: "Please report any issues or ideas to improve the tool to",
+    faq_a3_1: "konrad.korf@expertsender.com"  
+  },
+  pl: {
+    m1: "BUStool",
+    m2: "Instrukcje DNS",
+    m3: "Wiadomość transferowa",
+    m4: "Weryfikacja DNSów",
+    m5: "Rozgrzewka",
+    l1: "Imię:",
+    l2: "Domena główna",
+    l3: "Domena maskująca:",
+    l4: "Pierwszy IP:",
+    l5: "Drugi IP:",
+    l6: "Trzeci IP:",
+    l7: "Czwarty IP:",
+    l8: "Znacznik spf:",
+    l9: "Instancja:",
+    l10: "Kraj:",
+    l11: "Stany",
+    l12: "Chiny",
+    l13: "Brazylia",
+    l14: "Rosja",
+    l15: "Inny",
+    l16: "B2B",
+    l17: "google-site-verification:",
+    l18: "DKIM:",
+    l19: "Rodzaj skrzynki:",
+    l20: "Skrzynka IQ",
+    l21: "Własna",
+    dns_english_btn: "[EN] dedykowane IP",
+    dns_dedicated_btn: "[PL] dedykowane IP",
+    dns_shared_btn: "[PL] współdzielone IP",
+    dns_verification: "Weryfikacja DNSów",
+    temp_pass_label: "hasło do ExS:",
+    iq_pass_label: "hasło do IQ:",
+    pl: "Polska",
+    us: "Stany Zjednoczone",
+    uk: "Wielka Brytania",
+    fr: "Francja",
+    ru: "Rosja",
+    it: "Włochy",
+    ch: "Chiny",
+    de: "Niemcy",
+    cz: "Czechy",
+    in: "MIĘDZYNARODOWY",
+    bustype: "Rodzaj biznesu:",
+    tr_en_transfer: "[EN] Wiadomość transferowa",
+    tr_pl_transfer: "[PL] Wiadomość transferowa",
+    faq_q1: "Z jakiej przeglądarki korzystać?",
+    faq_q2: "Jak tego w ogóle używać?",
+    faq_q3: "Mi nie działa!",
+    faq_h1: "Google Chrome",
+    faq_h2: "Z rozsądkiem!",
+    faq_h3: "Zgłoś to!",
+    faq_a1: "Z nieznanych przyczyn klient pocztowy Outlooka działa lepiej z chrome'm, niż z firefox'em. Kopiowanie tabelek i zachowywanie styli będzie możliwe jedynie korzystając z chrome'a.",
+    faq_a2: "Narzędzie to powstało, aby usprawnić proces tworzenia nowego unitu dla klienta. Jest to proste narzędzie bez walidacji pól tekstowych, zatem należy zachować czujność, wprowadzając dane. Pozsotałe rzeczy, które należy wziąć pod uwagę:",
+    faq_a2_1:"1. Sprawdzić obecne tagi SPF. Jeżeli klient już posiada rekord, należy jedynie dodać nasz tag do niego. Jeśli brak jest rekordu, możemy pozostać przy wersji z instrukcji",
+    faq_a2_2:"2. Sprawdzić, czy wszystkie rekordy są poprawne, używając zewnętrznych narzędzi, np. DNSWatch",
+    faq_a2_3:"3.Tworząc rekord DKIM do instrukcji DNS, nalezy usunąć znak '/' z jej treści, aby w zakładce 'weryfikacja DNS' jednostki biznesowej weryfikacja przebiegła pomyślnie,",
+    faq_a2_4: "4.  Obecnie jest narzucona sztywna ilość IPików w instrukcji, jest w planach wersja z ilością IPików, podawanych przez użytkownika",
+    faq_a3: 'Jeśli zauważysz jakiś błąd lub masz sugestię dotyczącą tego narzędzia napisz do:',
+    faq_a3_1: "konrad.korf@expertsender.com"
+  },
+  ru: {
+    m1: "BUStool",
+    m2: "Инструкции DNS",
+    m3: "Передача сообщения",
+    m4: "Проверка DNS",
+    m5: "Разминка",
+    l1: "имя:",
+    l2: "Доменное имя:",
+    l3: "Маскирующий домен:",
+    l4: "Первый IP:",
+    l5: "Второй IP:",
+    l6: "Третий IP:",
+    l7: "Четвертый IP:",
+    l8: "Spf tag:",
+    l9: "Сервис:",
+    l10: "Страна:",
+    l11: "США",
+    l12: "Китай",
+    l13: "Бразилия",
+    l14: "Россия",
+    l15: "Другое",
+    l16: "B2B",
+    l17: "проверка google-сайта:",
+    l18: "DKIM:",
+    l19: "Тип почтового ящика:",
+    l20: "Настройка IQ",
+    l21: "Собственный",
+    dns_english_btn: "[EN] выделенные IP-адреса",
+    dns_dedicated_btn: "[PL] выделенные IP-адреса",
+    dns_shared_btn: "[PL] разделяет IP-адреса",
+    dns_verification: "Проверка DNS",
+    temp_pass_label: "ExS пароль:",
+    iq_pass_label: "IQ пароль:",
+    pl: "Польша",
+    us: "Соединенные Штаты Америки",
+    uk: "Великобритания",
+    fr: "Франция",
+    ru: "Россия",
+    it: "Италия",
+    ch: "Китай",
+    de: "Германия",
+    cz: "Чешская Республика",
+    in: "международный",
+    bustype: "Тип бизнеса",
+    tr_en_transfer: "[EN] передача сообщения",
+    tr_pl_transfer: "[PL] передача сообщения",
+    faq_a1: "По неизвестным причинам клиент электронной почты Outlook работает лучше с хром, чем с firefox. Копирование таблиц и сохранение стилей возможно только с помощью хрома ",
+    faq_a2: "Этот инструмент был создан, чтобы упростить процесс создания нового клиента для блока Это простой инструмент, без проверки текстовых полей, так что будьте бдительны, ввод данных Pozsotałe вещей, которые вы должны рассмотреть следующие вопросы:",
+    faq_a2_1: "1. Проверить текущие теги SPF. Если у клиента уже есть запись, просто добавьте к нему наш тег. Если нет записи, мы можем остаться с версией из инструкций",
+    faq_a2_2: "2. Убедитесь, что все записи верны с использованием внешних инструментов, например DNSWatch",
+    faq_a2_3: "3. При создании записи DKIM для инструкции DNS удалите символ '/' из его содержимого, чтобы проверка прошла успешно на вкладке 'Проверка DNS' бизнес-единицы",
+    faq_a2_4: "4. В настоящее время в инструкциях имеется жесткое количество IP-адресов, есть планы по версии с количеством IP-адресов, предоставленных пользователем",
+    faq_a3: "Пожалуйста, сообщайте о любых проблемах или идеи, которые улучшают инструмент + <a href=mailto:konrad.korf@expertsender.com> konrad.korf@expertsender.com </a>",
+    faq_a3: 'Если вы видите ошибку или у вас есть предложение для этого инструмента, напишите:',
+    faq_a3_1: "konrad.korf@expertsender.com"
+  },
+  pt: {
+    m1: "BUStool",
+    m2: "Instruções DNS",
+    m3: "Mensagem de transferência",
+    m4: "Verificação DNS",
+    m5: "Aquecer",
+    l1: "Primeiro nome:",
+    l2: "Nome do domínio:",
+    l3: "Masking domain:",
+    l4: "Primeiro IP:",
+    l5: "Segundo IP:",
+    l6: "Terceiro IP:",
+    l7: "Quarto IP:",
+    l8: "Spf tag:",
+    l9: "Serviço:",
+    l10: "País:",
+    l11: "EUA",
+    l12: "China",
+    l13: "Brasil",
+    l14: "Rússia",
+    l15: "Outros",
+    l16: "B2B",
+    l17: "google-site-verification:",
+    l18: "DKIM:",
+    l19: "Tipo de caixa de correio:",
+    l20: "onfiguração de IQ",
+    l21: "Próprio",
+    dns_english_btn: "[EN] dedicados IPs",
+    dns_dedicated_btn: "[PL] dedicados IPs",
+    dns_shared_btn: "[PL] compartilhados IPs",
+    dns_verification: "DNS verificação",
+    temp_pass_label: "ExS senha:",
+    iq_pass_label: "IQ senha:",
+    pl: "Polónia",
+    us: "Estados Unidos",
+    uk: "Reino Unido",
+    fr: "França",
+    ru: "Rússia",
+    it: "Itália",
+    ch: "China",
+    de: "Alemanha",
+    cz: "República Checa",
+    in: "INTERNATIONAL",
+    bustype: "Tipo de Negócio",
+    tr_en_transfer: "[EN] Mensagem de transferência",
+    tr_pl_transfer: "[PL] Mensagem de transferência",
+    faq_q1: "Qual navegador usar?",
+    faq_q2: "Como usar o site?",
+    faq_q3: "Algo está errado!",
+    faq_h1: "Google Chrome",
+    faq_h2: "Sabiamente",
+    faq_h3: "Denuncie-lo!",
+    faq_a1: "O Outlook, por algum motivo, interage melhor com o Chrome do que com o Firefox. Portanto, copiar uma tabela com estilos só estará disponível ao usar o Google Chrome.",
+    faq_a2: "O BUStool foi feito para ajudá-lo com o Business Unit Setup, no entanto, o não possui campos de validação de dados. Certifique-se de levar em consideração o seguinte:",
+    faq_a2_1:"1. Certifique-se de verificar registros SPF anteriores para clientes de comércio eletrônico e ajuste os registros às necessidades de um cliente,",
+    faq_a2_2:"2. Sempre verifique se todos os dados estão corretos com o função de verificação de DNS",
+    faq_a2_3:"3. Na pagina de verificação de DNS, você precisará excluir manualmente o sinal '/' da chave DKIM para que funcione no painel 'verify DNS' em ExpertSender",
+    faq_a2_4: "4. Há apenas uma versão para quatro IPs nas instruções do DNS, certifique-se de verificar quantos IPs estão sendo atribuídos e ajuste de acordo",
+    faq_a3: "Por favor, relate quaisquer problemas ou idéias para melhorar o BUStool para",
+    faq_a3_1: "konrad.korf@expertsender.com"  
+  }
+}; 
+
+//alert(translate.ru.m3).Innerhtml;
+
+
+$(document).ready(function(){
+  $("#lang").change(function(){
+    var i18n =translate;
+    var lang = $(this).val();
+      $("#nav1").text(i18n[lang].m1);
+      $("#nav2").text(i18n[lang].m2);
+      $("#nav3").text(i18n[lang].m3);
+      $("#nav4").text(i18n[lang].m4);
+      $("#nav5").text(i18n[lang].m5);
+      $("#l1").text(i18n[lang].l1);
+      $("#l2").text(i18n[lang].l2);
+      $("#l3").text(i18n[lang].l3);
+      $("#l4").text(i18n[lang].l4);
+      $("#l5").text(i18n[lang].l5);
+      $("#l6").text(i18n[lang].l6);
+      $("#l7").text(i18n[lang].l7);
+      $("#l8").text(i18n[lang].l8);
+      $("#l9").text(i18n[lang].l9);
+      $("#l10").text(i18n[lang].l10);
+      $("#l11").text(i18n[lang].l11);
+      $("#l12").text(i18n[lang].l12);
+      $("#l13").text(i18n[lang].l13);
+      $("#l14").text(i18n[lang].l14);
+      $("#l15").text(i18n[lang].l15);
+      $("#l16").text(i18n[lang].l16);
+      $("#l17").text(i18n[lang].l17);
+      $("#l18").text(i18n[lang].l18);
+      $("#l19").text(i18n[lang].l19);
+      $("#l20").text(i18n[lang].l20);
+      $("#l21").text(i18n[lang].l21);
+      $("#dns_english_btn").text(i18n[lang].dns_english_btn);
+      $("#dns_dedicated_btn").text(i18n[lang].dns_dedicated_btn);     
+      $("#dns_shared_btn").text(i18n[lang].dns_shared_btn);     
+      $("#dns_verification").text(i18n[lang].dns_verification);
+      $("#pl").text(i18n[lang].pl);
+      $("#us").text(i18n[lang].us);
+      $("#uk").text(i18n[lang].uk);
+      $("#ru").text(i18n[lang].ru);
+      $("#it").text(i18n[lang].it);
+      $("#ch").text(i18n[lang].ch);
+      $("#de").text(i18n[lang].de);
+      $("#cz").text(i18n[lang].cz);
+      $("#in").text(i18n[lang].in);
+      $("#temp_pass_label").text(i18n[lang].temp_pass_label);
+      $("#iq_pass_label").text(i18n[lang].iq_pass_label);
+      $("#bustype").text(i18n[lang].bustype);
+      $("#multiple_dns").text(i18n[lang].m4);
+      $("#tr_en_transfer").text(i18n[lang].tr_en_transfer);
+      $("#tr_pl_transfer").text(i18n[lang].tr_pl_transfer);
+      $("#warmup_btn").text(i18n[lang].m5);
+      $("#faq_q1").text(i18n[lang].faq_q1);
+      $("#faq_q2").text(i18n[lang].faq_q2);
+      $("#faq_q3").text(i18n[lang].faq_q3);
+      $("#faq_h1").text(i18n[lang].faq_h1);
+      $("#faq_h2").text(i18n[lang].faq_h2);
+      $("#faq_h3").text(i18n[lang].faq_h3);
+      $("#faq_a1").text(i18n[lang].faq_a1);
+      $("#faq_a2").text(i18n[lang].faq_a2);
+      $("#faq_a2_1").text(i18n[lang].faq_a2_1);
+      $("#faq_a2_2").text(i18n[lang].faq_a2_2);
+      $("#faq_a2_3").text(i18n[lang].faq_a2_3);
+      $("#faq_a2_4").text(i18n[lang].faq_a2_4);
+      $("#faq_a3").text(i18n[lang].faq_a3);
+      $("#faq_a3_1").text(i18n[lang].faq_a3_1);
+  })
+});
